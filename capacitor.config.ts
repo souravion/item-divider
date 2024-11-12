3ef353700196d8ace7bbc10cc5e10fa32ae10a6c
souravion <sourav.ion@gmail.com>
2024-11-12 06:13:42 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Item Divider',
+  webDir: 'www'
+};
+
+export default config;
